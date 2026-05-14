8c7a1eb209e87312c6e57e93ba83b10e8a67286b
MD Bayzid Hosen <mdbayazid131@gmail.com>
2026-05-14 21:41:35 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/which_win/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.which_win
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class WhichWin extends StatelessWidget {
+  const WhichWin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
